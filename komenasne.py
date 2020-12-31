6620fbf8d345b3f222200847d646322db4076522
nyumen <76697631+nyumen@users.noreply.github.com>
2020-12-31 16:41:24 +0900

    kako log api

--- komenasne.py
+++ komenasne.py
@@ -11,31 +11,30 @@ import configparser
 import platform
 import os
 import sys
+import tempfile
+import math
 
 '''
 # 必要
 pip install requests
 pip install beautifulsoup4
 iniの設定
-
-# メモ
-jk1=NHK 総合，jk2=Eテレ，jk4=日本テレビ，jk5=テレビ朝日，jk6=TBS テレビ，jk7=テレビ東京，jk8=フジテレビ
-，jk9=TOKYO MX，jk10=テレ玉，jk11=tvk，jk12=チバテレビ
-
-jk101=NHKBS-1，jk103=NHK BSプレミアム，jk141=BS 日テレ，jk151=BS 朝日，jk161=BS-TBS，jk171=BSジャパン
-，jk181=BSフジ，jk191=WOWOWプライム，jk192=WOWOWライブ，jk193=WOWOWシネマ，jk200=スターチャンネル1
-，jk201=スターチャンネル2，jk202=スターチャンネル3，jk211=BSイレブン
 '''
 
 jkchs = {
-    'ＢＳ１１' : 211,
-    'ＮＨＫＢＳプ' : 103,
     'ＮＨＫＢＳ１' : 101,
-    'ＢＳジャパン' : 171,
-    'ＢＳ朝日' : 151,
+    'ＮＨＫＢＳプ' : 103,
     'ＢＳ日テレ' : 141,
+    'ＢＳ朝日' : 151,
     'ＢＳ－ＴＢＳ' : 161,
+    'ＢＳジャパン' : 171,
+    'ＢＳテレ東' : 171,
     'ＢＳフジ' : 181,
+    'ＷＯＷＯＷプライム' : 191,
+    'スターチャンネル１' : 200,
+    'ＢＳ１１' : 211,
+    'ＢＳ１２' : 222,
+    'ＢＳアニマックス' : 236,
     '総合' : 1,
     'Ｅテレ' : 2,
     '日テレ' : 4,
@@ -67,10 +66,21 @@ jk_names = {
     'jk7' : 'テレビ東京',
     'jk8' : 'フジテレビ',
     'jk9' : 'TOKYO MX',
-    'jk211' : 'BS11'}
+    'jk101' : 'NHK BS1',
+    'jk103' : 'NHK BSプレミアム',
+    'jk141' : 'BS日テレ',
+    'jk151' : 'BS朝日',
+    'jk161' : 'BS-TBS',
+    'jk171' : 'BSテレ東',
+    'jk181' : 'BSフジ',
+    'jk191' : 'WOWOWプライム',
+    'jk200' : 'BSスター1',
+    'jk211' : 'BS11イレブン',
+    'jk222' : 'BS12トゥエルビ',
+    'jk236' : 'BSアニマックス'}
 
 def get_item(ip_addr, playing_content_id):
-    get_title_lists = s.get(f'http://' + ip_addr + ':64220/recorded/titleListGet?searchCriteria=0&filter=0&startingIndex=0&requestedCount=0&sortCriteria=0&withDescriptionLong=0&withUserData=0')
+    get_title_lists = s.get(f'http://{ip_addr}:64220/recorded/titleListGet?searchCriteria=0&filter=0&startingIndex=0&requestedCount=0&sortCriteria=0&withDescriptionLong=0&withUserData=0')
     title_lists = json.loads(get_title_lists.text)
 
     for item in title_lists['item']:
@@ -86,6 +96,7 @@ def get_jkid(channel_name):
 def get_datetime(date_time):
     return datetime.datetime.strptime(date_time, "%Y-%m-%dT%H:%M:%S+09:00")
 
+# タイムシフトのURL（watch/lv****** ）を取得
 def get_tsurl(jkid, date_time):
     # "TBSテレビ【ニコニコ実況】2020年12月26日"等
     ch_name = jk_names[jkid]
@@ -93,12 +104,51 @@ def get_tsurl(jkid, date_time):
     print(enc_title)
     load_url = "https://live.nicovideo.jp/search?keyword=" + quote(enc_title) + "&status=onair&sortOrder=recentDesc&providerTypes=channel"
 
-    html = requests.get(load_url)
+    html = requests.get(load_url,headers=headers)
     soup = BeautifulSoup(html.content, "html.parser")
 
-    # タイムシフトのURL（watch/lv****** ）を取得
     param = soup.find(class_="searchPage-ProgramList_TitleLink").get('href')
     return 'https://live2.nicovideo.jp/' + param
+
+# ファイル名に使用できない文字を変換
+def replace_title(title):
+    title = title.replace('\ue192', '[再]')
+    title = title.replace('\ue193', '[新]')
+    title = title.replace('\ue195', '[終]')
+    title = title.replace('\ue0fe', '[字]')
+    title = title.replace('\ue183', '[多]')
+    title = title.replace('\ue18c', '[映]')
+    title = title.replace('\ue18d', '[無]')
+    title = title.replace('\ue2ca1', 'No1')
+    title = title.replace('/', '／')
+    title = title.replace('<', '＜')
+    title = title.replace('>', '＞')
+    title = title.replace(':', '：')
+    return title
+
+# vposをdateとdate_usecから再計算する（commeon対策）
+def rewrite_vpos(start_date_unixtime, xml_line):
+    vpos_start = xml_line.find(' vpos="')
+    if vpos_start == -1:
+        return xml_line
+    # xml内のvposの値を取得
+    vpos_pos = vpos_start + 7 # len(' vpos="')
+    vpos_str_count = xml_line[vpos_pos:].find('"')
+    vpos_num = int(xml_line[vpos_pos:vpos_pos + vpos_str_count])
+    # xml内dateの値の取得
+    date_start = xml_line.find(' date="')
+    date_num = xml_line[date_start + 7:date_start + 17]
+    # xml内date_usecの値の取得
+    date_usec_start = xml_line.find(' date_usec="')
+    if date_usec_start == -1:
+        date_usec_num = "0"
+    else:
+        date_usec_str_count = xml_line[date_usec_start + 12:].find('"')
+        date_usec_num = xml_line[date_usec_start + 12:date_usec_start + 12 + date_usec_str_count]
+    # コメントのunixtimeから動画開始時のunixtime引いた値を新しいvposとする
+    comment_unixtime = float(date_num + "." + date_usec_num)
+    new_vpos = math.ceil((comment_unixtime - start_date_unixtime) * 100)
+    return xml_line[:vpos_pos] + str(new_vpos) + xml_line[vpos_pos + vpos_str_count:]
 
 
 # init
@@ -107,13 +157,21 @@ ini.read('./komenasne.ini', 'UTF-8')
 nase_ini = ini['NASNE']['ip']
 nasne_ips = [x.strip() for x in nase_ini.split(',')]
 
-jkcommentviewer_path = None
-if ini['PLAYER']['jkcommentviewer_path']:
-    jkcommentviewer_path = ini['PLAYER']['jkcommentviewer_path']
-    if platform.platform().startswith("Windows"):
-        jkcommentviewer_path = jkcommentviewer_path.replace(os.sep, os.sep + os.sep)
+is_windows = platform.platform().startswith("Windows")
+
+try:
+    commeon_path = ini['PLAYER']['commeon_path']
+except KeyError:
+    commeon_path = None
+if commeon_path and is_windows:
+    if ini['PLAYER']['commeon_path']:
+        commeon_path = ini['PLAYER']['commeon_path']
+        commeon_path = commeon_path.replace(os.sep, os.sep + os.sep)
+        kakolog_dir = ini['LOG']['kakolog_dir']
+        kakolog_dir = kakolog_dir.replace(os.sep, os.sep + os.sep)
 
 s = requests.Session();
+headers = {'user-agent':'komenasne'}
 
 # main
 for ip_addr in nasne_ips:
@@ -125,7 +183,9 @@ for ip_addr in nasne_ips:
         playing_content_id = playing_info['client'][0]['content']['id']
         item = get_item(ip_addr, playing_content_id)
         #print(item)
-        print(item['id'] + ' ' + item['title'] + ' ' + item['channelName'])
+        #print(item['title'].encode('unicode-escape'))
+        title = replace_title(item['title'])
+        print(item['id'] + ' ' + title + ' ' + item['channelName'])
         jkid = get_jkid(item['channelName'])
         if not jkid:
              print('エラー：「' + item['channelName'] + '」は定義されていないチャンネルのため、連携できません。')
@@ -134,15 +194,17 @@ for ip_addr in nasne_ips:
         start_date_time = get_datetime(item['startDateTime'])
         end_date_time = start_date_time + datetime.timedelta(seconds=item['duration'])
         print("start:" + str(start_date_time), "end:" + str(end_date_time))
+        start_unixtime = start_date_time.timestamp()
+        end_unixtime = end_date_time.timestamp()
 
-        ts_time = start_date_time - datetime.timedelta(hours=4)
-        try:
-            url = get_tsurl(jkid, ts_time)
-        except:
-             print('エラー：タイムシフト番組が見つかりません。')
-             sys.exit(1)
-
-        if jkcommentviewer_path is None:
+        if not is_windows or commeon_path is None:
+            # ブラウザ用のコメント再生処理、Windows・Mac兼用
+            ts_time = start_date_time - datetime.timedelta(hours=4)
+            try:
+                url = get_tsurl(jkid, ts_time)
+            except:
+                print('エラー：タイムシフト番組が見つかりません。')
+                sys.exit(1)
             if ts_time.strftime('%Y%m%d') == '20201216':
                 # 新ニコニコ実況の初日は11時開始のため7時間減算
                 ts_time = ts_time - datetime.timedelta(hours=7)
@@ -151,12 +213,19 @@ for ip_addr in nasne_ips:
             print(browser_url)
             webbrowser.open(browser_url)
         else:
-            jkcommentviewer_url = url + '?start_date=' + start_date_time.strftime("%Y%m%d%H%M%S") + '&end_date=' + end_date_time.strftime("%Y%m%d%H%M%S")
-            path = jkcommentviewer_path + " " + jkcommentviewer_url
-            print(path)
-            subprocess.Popen([jkcommentviewer_path, jkcommentviewer_url])
+            # commeon用のコメント再生処理
+            kakolog = s.get(f'https://jikkyo.tsukumijima.net/api/kakolog/{jkid}?starttime={start_unixtime}&endtime={end_unixtime}&format=xml',headers=headers)
+            logfile = kakolog_dir + jk_names[jkid] + '_' + start_date_time.strftime("%Y%m%d_%H%M%S") + '_' + title + '.xml'
+            with open(logfile, 'w', encoding="utf-8") as saveFile:
+                start_date_unixtime = start_date_time.timestamp()
+                for xml_line in kakolog.iter_lines():
+                    line = rewrite_vpos(start_date_unixtime, xml_line.decode())
+                    saveFile.write(line + '\n')
+                    if line == '<error>指定された期間の過去ログは存在しません。</error>':
+                        print('エラー：指定された期間の過去ログは存在しません。')
+                        sys.exit(1)
+            subprocess.Popen([commeon_path, logfile])
 
-        break
         sys.exit(0)
 
 print('エラー：再生中のnasneの動画が見つからないため、終了します。')
