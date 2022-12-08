e96317bc5c84775d5b1ca1ea3e31cadcedfe6277
nyumen <kam@mbf.nifty.com>
2022-12-08 20:56:55 +0900

    v1.1 update

--- src/build.bat
+++ src/build.bat
@@ -1,3 +1,18 @@
-pyinstaller komenasne.spec --onefile --clean
+rm komenasne.exe
+mkdir exe
+cd exe
+pipenv --rm
+pipenv --python 3.10.5
+pipenv shell
+REM ↓続きはこっからコピペ
+pipenv install requests
+pipenv install beautifulsoup4
+pipenv install python-dateutil
+pipenv install tweepy
+pip install pyinstaller
+copy ..\komenasne.py .
+copy ..\komenasne.ico .
+pyinstaller komenasne.py --icon=komenasne.ico --onefile --clean
 move dist\komenasne.exe ..\
+pipenv --rm
 pause
